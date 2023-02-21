832ac50c9848c14c724132aeecf61ea90955751e
Choi <seongwon.choi@lgcns.com>
2023-02-21 10:49:33 +0900

    f c

---
+++ src/pages/create.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function Create() {
+  return (
+    <>
+      <h1>
+        <Link href="./">WEB</Link>
+      </h1>
+      <ol>
+        <li>
+          <Link href="/read/1">htm1</Link>
+        </li>
+        <li>
+          <Link href="/read/2">css</Link>
+        </li>
+      </ol>
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB!!
+      </article>
+      <ul>
+        <li>
+          <Link href="/create">Create</Link>
+        </li>
+        <li>
+          <Link href="/update">Update</Link>
+        </li>
+        <li>
+          <Link href="/delete">Delete</Link>
+        </li>
+      </ul>
+    </>
+  );
+}
